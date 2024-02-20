72c54b0a7327b36e941a6c320777237d2dfa18e6
Štěpán Velínský <stteranv@gmail.com>
2024-02-20 22:16:06 +0100

    Desing

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import questions from "./data"
 import Question from "./components/Question"
 
 const App = () => {
-
-  return <>
-    <Question />
-  </>
+  return <div className="all-questions">
+    {
+      questions.map( (oneQuestion) => {
+        return <Question key={oneQuestion.id} {...oneQuestion}/>
+      })
+    }
+  </div>
 }
 
 export default App
--- src/components/Question.js
+++ src/components/Question.js
@@ -3,14 +3,13 @@ import { useState } from "react"
 
 const Question = ({title, info}) => {
     const [show, setShow] = useState(false)
-    return<>
+    return<div className="one-question">
         <section>
             <h2>{title}</h2>
             <button onClick={ () => setShow(!show) }>Odpověď</button>
         </section>
         {show && <p>{info}</p>}
-        
-    </>
+    </div>
 }
 
 export default Question
